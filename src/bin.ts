63c907a59d2d7f41b2f93ec80204a7c1c8da2790
unleashy <unleashy@users.noreply.github.com>
2024-09-06 20:48:38 -0300

    Select random points within countries

--- src/bin.ts
+++ src/bin.ts
@@ -1,3 +1,17 @@
-import { getCountryData } from "./index.ts";
+import {
+  getCountryData,
+  selectRandomCountry,
+  mapToCountry,
+  attempt,
+} from "./index.ts";
 
-let cd = await getCountryData(import.meta.dirname + "/data/countries.json.br");
+const MAX_ATTEMPTS = 1000;
+
+let countryData = await getCountryData(
+  import.meta.dirname + "/data/countries.json.br",
+);
+
+let country = selectRandomCountry(countryData, Math.random());
+let position = attempt(MAX_ATTEMPTS, () =>
+  mapToCountry(country, [Math.random(), Math.random()]),
+);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,51 @@
 import fs from "node:fs";
 import zlib from "node:zlib";
 import { json } from "node:stream/consumers";
-import { type MultiPolygon, type Polygon } from "geojson";
+import { type MultiPolygon, type Polygon, type Position } from "geojson";
+import { booleanPointInPolygon, bbox } from "@turf/turf";
+
+export type CountryPolygon = Polygon | MultiPolygon;
 
 export interface CountryData {
-  [iso: string]: Polygon | MultiPolygon;
+  readonly [iso: string]: CountryPolygon;
 }
 
-export function getCountryData(path: string): Promise<CountryData> {
-  return json(
+export async function getCountryData(path: string): Promise<CountryData> {
+  let cd = await json(
     fs.createReadStream(path).pipe(zlib.createBrotliDecompress()),
-  ) as Promise<CountryData>;
+  );
+
+  return Object.freeze(cd);
+}
+
+export function selectRandomCountry(
+  cd: CountryData,
+  r: number,
+): CountryPolygon {
+  let cs = Object.keys(cd);
+  let i = Math.floor(denorm(r, 0, cs.length));
+
+  return cd[cs[i]];
+}
+
+export function mapToCountry(
+  c: CountryPolygon,
+  [rlat, rlong]: Position,
+): Position | undefined {
+  let b = bbox(c);
+  let attempt = [denorm(rlat, b[0], b[2]), denorm(rlong, b[1], b[3])];
+  return booleanPointInPolygon(attempt, c) ? attempt : undefined;
+}
+
+function denorm(r: number, min: number, max: number): number {
+  return min + r * (max - min);
+}
+
+export function attempt<T>(max: number, f: () => T | undefined): T | undefined {
+  for (let i = 0; i < max; i++) {
+    let result = f();
+    if (result !== undefined) return result;
+  }
+
+  return undefined;
 }
